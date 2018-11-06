f9ee3cf2ada281ba9b6e1a8f802b6a88417f701a
HannaStrezh <hannastrezh@gmail.com>
2018-11-06 14:48:08 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize #why we don't pass the knowledge as an argument#
+   @knowledge =[]
+  end
+
+  def learn
+student.knowledge.map do |st_knowledge|
+
+  end
 end
+
+
+end#end of a Student class
