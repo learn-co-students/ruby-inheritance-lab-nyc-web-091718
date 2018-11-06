617b9a0e03cad21a15d43c5d1fe4e09acdaf97ee
HannaStrezh <hannastrezh@gmail.com>
2018-11-06 15:41:59 -0500

    completed ruby-inheritance lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,15 +1,19 @@
+require_relative "../lib/user.rb"
 class Student < User
-  attr_accessor :knowledge
+  #attr_accessor :knowledge
 
   def initialize #why we don't pass the knowledge as an argument#
    @knowledge =[]
   end
 
-  def learn
-student.knowledge.map do |st_knowledge|
-
+  def learn(string)
+    @knowledge << string
+# Student.knowledge.map do |st_knowledge|
   end
-end
+  
+  def knowledge
+    @knowledge
+  end
 
 
 end#end of a Student class
